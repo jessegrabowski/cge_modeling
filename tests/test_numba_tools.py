af185019a3ef25f7832f68ac44256ef377329673
jessegrabowski <jessegrabowski@gmail.com>
2024-01-26 09:34:51 +0100

    Refactor `numba_lambdify` so generated function signatures match pytensor functions (all backends now agree)

---
+++ tests/test_numba_tools.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pytest
+import sympy as sp
+
+from cge_modeling.tools.numba_tools import numba_lambdify
+
+
+def test_numba_lambdify_single_output():
+    a, b, c = sp.symbols("a b c")
+    f = a * b + c
+    f_func = numba_lambdify(inputs=[a, b, c], outputs=[f])
+
+    assert f_func(1, 2, 3) == 5
+
+
+@pytest.mark.parametrize("stack", [True, False], ids=["stack", "no_stack"])
+def test_numba_lambdify_multi_output(stack):
+    a, b, c = sp.symbols("a b c")
+    f1 = a * b + c
+    f2 = a + b + c
+
+    f_func = numba_lambdify(inputs=[a, b, c], outputs=[f1, f2], stack_outputs=stack)
+
+    outputs = f_func(1, 2, 3)
+    if not stack:
+        assert outputs[0] == 5
+        assert outputs[1] == 6
+    else:
+        np.testing.assert_allclose(outputs, np.array([5, 6]))
+
+
+def test_numba_lambdify_matrix_output():
+    a, b, c = sp.symbols("a b c")
+    f1 = sp.Matrix([a * b + c, a + b + c])
+
+    f_func = numba_lambdify(inputs=[a, b, c], outputs=[f1])
+
+    outputs = f_func(1, 2, 3)
+    np.testing.assert_allclose(outputs, np.array([5, 6]).reshape((2, 1)))
