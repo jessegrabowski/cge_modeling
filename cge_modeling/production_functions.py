f6583d93392a51302f9439d2e982e1e206305151
jessegrabowski <jessegrabowski@gmail.com>
2024-01-29 16:39:28 +0100

    Remove obsolete `leontief` and `cobb-douglass` functions from `production_functions.py`
    Use less opinionated names for `TFP` and `factor_shares` in production functions

--- cge_modeling/production_functions.py
+++ cge_modeling/production_functions.py
@@ -6,6 +6,8 @@ import sympy as sp
 
 from cge_modeling.tools.pytensor_tools import at_least_list
 
+BACKEND_TYPE = Literal["numba", "pytensor"]
+
 
 def _check_pairwise_lengths_match(names, args):
     list_args = [at_least_list(arg) for arg in args]
@@ -93,12 +95,12 @@ def _add_second_alpha(alpha: Union[str, list[str]], factors: Union[str, list[str
 
 
 def CES(
-    factors: Union[list[str], str],
-    factor_prices: Union[list[str], str],
+    factors: Union[list[str, ...], str],
+    factor_prices: Union[list[str, ...], str],
     output: str,
     output_price: str,
-    A: str,
-    alphas: str,
+    TFP: str,
+    factor_shares: Union[str, list[str, ...]],
     epsilon: str,
     *args,
     **kwargs,
@@ -111,7 +113,9 @@ def CES(
     .. math::
         Y = A \\left( \\sum_{i \\in I} \alpha_i X_i^{\frac{\\epsilon - 1}{\\epsilon}} \right)^{\frac{\\epsilon}{\\epsilon - 1}}
 
-    Which generates the following factor demands:
+    Where :math:`A` is the total factor productivity parameter, :math:`\\alpha_i` is the share of factor :math:`X_i` in
+    the production process, and :math:`\\epsilon` is the elasticity of substitution parameter. The profit maximization
+    problem for this production function generates the following factor demands:
 
     .. math::
         X_i = \frac{Y}{A} \\left( \frac{\alpha_i P_Y A}{P_i} \right)^{\\epsilon}
@@ -134,10 +138,10 @@ def CES(
     output_price: str
         Name of the price of the output
 
-    A: str
+    TFP: str
         Technology parameter
 
-    alphas: str, or list of str
+    factor_shares: str, or list of str
         Factor shares in the production function. If a string is provided and len(factors) == 2, the second factor
         share will be set to (1 - alpha). If a list of strings is provided, it must be the same length as factors.
 
@@ -153,24 +157,29 @@ def CES(
         output and factor equations
     """
 
-    alphas = _add_second_alpha(alphas, factors)
+    factor_shares = _add_second_alpha(factor_shares, factors)
     _check_pairwise_lengths_match(
-        ["factors", "factor_prices", "alpha"], [factors, factor_prices, alphas]
+        ["factors", "factor_prices", "factor_shares"], [factors, factor_prices, factor_shares]
     )
 
-    production_inner_template = Template("($alpha) * $factor ** (($epsilon - 1) / $epsilon)")
+    production_inner_template = Template(
+        "($factor_shares) * $factor ** (($epsilon - 1) / $epsilon)"
+    )
 
     production_inner = [
-        production_inner_template.safe_substitute(factor=factor, alpha=alpha, epsilon=epsilon)
-        for factor, alpha in zip(factors, alphas)
+        production_inner_template.safe_substitute(
+            factor=factor, factor_shares=factor_share, epsilon=epsilon
+        )
+        for factor, factor_share in zip(factors, factor_shares)
     ]
 
     eq_production = Template(
-        "$output = $A * ($inner) ** ($epsilon / ($epsilon - 1))"
-    ).safe_substitute(output=output, inner=" + ".join(production_inner), A=A, epsilon=epsilon)
+        "$output = $TFP * ($inner) ** ($epsilon / ($epsilon - 1))"
+    ).safe_substitute(output=output, inner=" + ".join(production_inner), TFP=TFP, epsilon=epsilon)
 
     factor_demand_template = Template(
-        "$factor = $output / $A * (($alpha) * $output_price * $A / $factor_price) ** " "$epsilon"
+        "$factor = $output / $TFP * (($factor_share) * $output_price * $TFP / $factor_price) ** "
+        "$epsilon"
     )
     eq_fac_demands = [
         factor_demand_template.safe_substitute(
@@ -178,11 +187,11 @@ def CES(
             factor_price=factor_price,
             output=output,
             output_price=output_price,
-            alpha=alpha,
-            A=A,
+            factor_share=factor_share,
+            TFP=TFP,
             epsilon=epsilon,
         )
-        for factor, factor_price, alpha in zip(factors, factor_prices, alphas)
+        for factor, factor_price, factor_share in zip(factors, factor_prices, factor_shares)
     ]
 
     return (eq_production,) + tuple(eq_fac_demands)
@@ -196,9 +205,9 @@ def dixit_stiglitz(
     epsilon: str,
     dims: str,
     coords: dict[str, list[str]],
-    backend: Literal["numba", "pytensor"] = "numba",
-    A: Optional[str] = None,
-    alphas: Optional[str] = None,
+    backend: BACKEND_TYPE = "numba",
+    TFP: Optional[str] = None,
+    factor_shares: Optional[str] = None,
 ) -> tuple[str, str]:
     """
     Generate string equations representing a Dixit-Stiglitz production process.
@@ -216,6 +225,135 @@ def dixit_stiglitz(
     Many researchers omit either the technology parameter, the factor shares, or both. If the technology parameter is
     not provided (i.e. :math:`A` is None), it is assumed to be 1. If the factor shares are omitted they are also set
     to 1.
+
+    Parameters
+    ----------
+    factors: str or list of str
+        Production factor. If a list is provided, it must be of length 1.
+
+        .. warning::
+        Despite the plural name, the Dixit-Stiglitz production function expects only a single facto input. The name
+        is plural to be consistent with other production functions.
+
+    factor_prices: str or list of str
+        Production factor prices. If a list is provided, it must be of length 1.
+
+    output: str
+        Name of the output of the production function
+
+    output_price: str
+        Name of the price of the output
+
+    TFP: str, optional
+        Technology parameter. If not provided, it is omitted from the equations, which is equivalent to setting it to 1.
+
+    factor_shares: str, optional
+        Factor shares in the production function. If a list is provided, it must be of length 1. If not provided, it is
+        omitted from the equations, which is equivalent to setting it to 1.
+
+    epsilon:
+        Elasticity of substitution parameter
+
+    dims: Sequence of str or str
+        Sequence of named dimensions for variables in the equations to be generated. All dimensions should appear in
+        the coords dictionary as keys, otherwise an error will be raised.
+
+    coords: dict of str: list of str
+        Dictionary of coordinates for the model, mapping dimension names to lists of labels.
+
+    backend: str, one of 'numba' or 'pytensor'
+        Backend that will parse the equations. Only relevant for aggregation functions like Sum and Prod. Elementwise
+        operations are interoperable between backends.
+
+    Returns
+    -------
+    (variables, parameters, equations): tuple of tuples of strings
+        output and factor equations
+    """
+
+    for var, name in zip(
+        [factors, factor_prices, factor_shares], ["factors", "factor_prices", "factor_shares"]
+    ):
+        if isinstance(var, list) and len(var) != 1:
+            raise ValueError(
+                f"Dixit-Stiglitz production function expects only a single factor input, but provided "
+                f"{name} has length {len(var)}"
+            )
+
+    if isinstance(factors, list):
+        factors = factors[0]
+    if isinstance(factor_prices, list):
+        factor_prices = factor_prices[0]
+    if isinstance(factor_shares, list):
+        factor_shares = factor_shares[0]
+
+    share_str = "$factor_share * " if factor_shares is not None else ""
+    TFP_str = f"$TFP * " if TFP is not None else ""
+
+    kernel_str = f"{share_str}$factor ** (($epsilon - 1) / $epsilon)"
+
+    kernel_template = Template(kernel_str)
+    kernel = kernel_template.safe_substitute(
+        factor_share=factor_shares, factor=factors, epsilon=epsilon
+    )
+
+    dim_len = len(coords[dims]) - 1
+
+    if backend == "numba":
+        rhs_str = "Sum($kernel, ($dims, 0, $dim_len)) ** ($epsilon / ($epsilon - 1))"
+    elif backend == "pytensor":
+        rhs_str = "($kernel).sum() ** ($epsilon / ($epsilon - 1))"
+    else:
+        raise ValueError(f"backend must be one of 'numba' or 'pytensor', found {backend}")
+
+    production_function = Template(f"$output = {TFP_str}{rhs_str}").safe_substitute(
+        output=output, TFP=TFP, kernel=kernel, epsilon=epsilon, dims=dims, dim_len=dim_len
+    )
+
+    demand_template = f"$factor = $output / {TFP_str}({TFP_str}{share_str}$output_price / $factor_price) ** $epsilon"
+
+    factor_demands = Template(demand_template).safe_substitute(
+        factor=factors,
+        output=output,
+        TFP=TFP,
+        factor_share=factor_shares,
+        output_price=output_price,
+        factor_price=factor_prices,
+        epsilon=epsilon,
+    )
+
+    return production_function, factor_demands
+
+
+def leontief(
+    factors: Union[str, list[str]],
+    factor_prices: Union[str, list[str]],
+    output: str,
+    output_price: str,
+    factor_shares: Union[str, list[str]],
+    dims: str,
+    coords: dict[str, list[str]],
+    backend: Literal["numba", "pytensor"] = "numba",
+) -> tuple[str, str]:
+    """
+    Generate string equations representing a Leontief production process.
+
+    A Leontief production process is defined as:
+
+    .. math::
+        Y = \\min_i \\left( \\frac{X_i}{\\alpha_i} \\right)
+
+    Where :math:`\\alpha_i` is the share of factor :math:`i` in the production process. This generates the following
+    factor demands:
+
+    .. math::
+        X_i = \\alpha_i Y
+
+    Because of the non-linearity of the minimum operator, this function does not return the production function itself.
+    Insead, we return the equivalent zero-profit condition:
+
+    .. math::
+        P_Y Y = \\sum_i P_i X_i
 
     Parameters
     ----------
@@ -262,90 +400,4 @@ def dixit_stiglitz(
         output and factor equations
     """
 
-    for var, name in zip([factors, factor_prices, alphas], ["factors", "factor_prices", "alphas"]):
-        if isinstance(var, list) and len(var) != 1:
-            raise ValueError(
-                f"Dixit-Stiglitz production function expects only a single factor input, but provided "
-                f"{name} has length {len(var)}"
-            )
-
-    if isinstance(factors, list):
-        factors = factors[0]
-    if isinstance(factor_prices, list):
-        factor_prices = factor_prices[0]
-    if isinstance(alphas, list):
-        alphas = alphas[0]
-
-    alpha_str = "$alpha * " if alphas is not None else ""
-    A_str = f"$A * " if A is not None else ""
-
-    kernel_str = f"{alpha_str}$factor ** (($epsilon - 1) / $epsilon)"
-
-    kernel_template = Template(kernel_str)
-    kernel = kernel_template.safe_substitute(alpha=alphas, factor=factors, epsilon=epsilon)
-
-    dim_len = len(coords[dims]) - 1
-
-    if backend == "numba":
-        rhs_str = "Sum($kernel, ($dims, 0, $dim_len)) ** ($epsilon / ($epsilon - 1))"
-    elif backend == "pytensor":
-        rhs_str = "($kernel).sum() ** ($epsilon / ($epsilon - 1))"
-    else:
-        raise ValueError(f"backend must be one of 'numba' or 'pytensor', found {backend}")
-
-    production_function = Template(f"$output = {A_str}{rhs_str}").safe_substitute(
-        output=output, A=A, kernel=kernel, epsilon=epsilon, dims=dims, dim_len=dim_len
-    )
-
-    demand_template = (
-        f"$factor = $output / {A_str}({A_str}{alpha_str}$output_price / $factor_price) ** $epsilon"
-    )
-
-    factor_demands = Template(demand_template).safe_substitute(
-        factor=factors,
-        output=output,
-        A=A,
-        alpha=alphas,
-        output_price=output_price,
-        factor_price=factor_prices,
-        epsilon=epsilon,
-    )
-
-    return production_function, factor_demands
-
-
-def cobb_douglass(
-    output: sp.Symbol,
-    output_price: sp.Symbol,
-    tfp: sp.Symbol,
-    inputs: list[sp.Symbol],
-    input_prices: list[sp.Symbol],
-    shares: list[sp.Symbol],
-):
-    if len(inputs) != len(shares):
-        if (len(shares) + 1) != len(inputs):
-            raise ValueError("The length of the shares should len(inputs), or len(inputs) - 1")
-        shares.append(1 - sum(shares))
-
-    system = [output - tfp * sp.prod([x**a for x, a in zip(inputs, shares)])]
-    for factor, price, share in zip(inputs, input_prices, shares):
-        system.append(factor - share * output * output_price / price)
-
-    return system
-
-
-def leontief(
-    output: sp.Symbol,
-    output_price: sp.Symbol,
-    inputs: list[sp.Symbol],
-    input_prices: list[sp.Symbol],
-    params: list[sp.Symbol],
-):
-    if len(inputs) != len(params):
-        raise ValueError("The length of the params should be len(inputs)")
-
-    system = [output_price * output - sum(P * x for P, x in zip(input_prices, inputs))]
-    for factor, param in zip(inputs, params):
-        system.append(factor - param * output)
-
-    return system
+    pass
