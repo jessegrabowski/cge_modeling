d6346e52463f17fa4e7bb358e5660a7dc39751bc
Jesse Grabowski <jessegrabowski@gmail.com>
2024-05-22 16:46:42 +0800

    Use `CostFuncWrapper` in cross-entropy function to get progress bar

--- cge_modeling/tools/rebalance.py
+++ cge_modeling/tools/rebalance.py
@@ -8,11 +8,16 @@ import pytensor
 import pytensor.tensor as pt
 from scipy import optimize
 
+from cge_modeling.base.utilities import (
+    CostFuncWrapper,
+    _optimzer_early_stopping_wrapper,
+)
+from cge_modeling.tools.pytensor_tools import rewrite_pregrad
+
 _log = logging.getLogger(__name__)
 
 
 class SAMTransformer:
-
     __slots__ = ("_fit", "scale", "negative_mask")
 
     def __init__(self):
@@ -60,7 +65,12 @@ def get_flat_nonzero_values(df):
 
 
 def _crossentropy_method_setup(
-    df, use_grad=True, use_hess=False, use_hessp=True, use_constraint_grad=True, mode=None
+    df,
+    use_grad=True,
+    use_hess=False,
+    use_hessp=True,
+    use_constraint_grad=True,
+    mode=None,
 ) -> tuple[
     Callable,
     Callable,
@@ -78,12 +88,13 @@ def _crossentropy_method_setup(
 
     X = pt.zeros(df.shape)
     x = pt.dvector("x")
+
     X = pt.set_subtensor(X[df.values > 0], x)
     f_X = compile_fn([x], X)
 
     nonzero_values = get_flat_nonzero_values(df)
 
-    cross_entropy = (x * (pt.log(x) - pt.log(nonzero_values))).sum()
+    cross_entropy = (x * (pt.log(x + 1e-10) - pt.log(nonzero_values))).sum()
 
     constraint_1 = pt.linalg.norm(X.sum(axis=0) - X.sum(axis=1), ord=1)
     constraint_2 = x.sum() - 1
@@ -94,22 +105,28 @@ def _crossentropy_method_setup(
     f_2 = compile_fn([x], constraint_2)
 
     if use_constraint_grad:
+        rewrite_pregrad(constraint_1)
+        rewrite_pregrad(constraint_2)
+
         f_1_grad = compile_fn([x], pt.grad(constraint_1, x))
         f_2_grad = compile_fn([x], pt.grad(constraint_2, x))
 
     if use_grad:
+        rewrite_pregrad(cross_entropy)
         grad_ce = pt.grad(cross_entropy, x)
         f_grad = compile_fn([x], grad_ce)
 
     if use_hess:
         if not use_grad:
             raise ValueError("Must use gradient to compute Hessian")
+        rewrite_pregrad(grad_ce)
         hess_ce = pytensor.gradient.jacobian(grad_ce, x)
         f_hess = compile_fn([x], hess_ce)
 
     if use_hessp:
         if not use_grad:
             raise ValueError("Must use gradient to compute Hessian")
+        rewrite_pregrad(grad_ce)
         p = pt.dvector("p")
         jvp_ce = pytensor.gradient.Rop(grad_ce, x, p)
         f_jvp_ce = compile_fn([x, p], jvp_ce)
@@ -126,6 +143,7 @@ def balance_SAM(
     use_constraint_grad=True,
     mode=None,
     method="SLSQP",
+    progressbar=True,
     **minimize_kwargs,
 ):
     if how != "cross-entropy":
@@ -159,29 +177,45 @@ def balance_SAM(
 
     nonzero_values = get_flat_nonzero_values(normalized_sam)
     x0 = np.random.dirichlet(alpha=np.ones_like(nonzero_values))
+
     x0 = minimize_kwargs.pop("x0", x0)
     tol = minimize_kwargs.pop("tol", 1e-12)
     options = minimize_kwargs.pop("options", {})
     maxiter = options.pop("maxiter", 25_000)
     options.update(maxiter=maxiter)
 
-    res = optimize.minimize(
+    objective = CostFuncWrapper(
         f_ce,
-        x0=x0,
-        jac=f_grad,
+        args=None,
+        f_jac=f_grad,
+        f_hess=f_hess,
+        maxeval=maxiter,
+        progressbar=progressbar,
+        update_every=10,
+    )
+
+    f_optim = partial(
+        optimize.minimize,
+        objective,
+        x0,
+        jac=use_grad,
+        hess=objective.f_hess if use_hess else None,
+        callback=objective.callback,
         hessp=f_jvp_ce,
         constraints=[  # noqa
             {"type": "eq", "fun": f_1, "jac": f_1_grad},
             {"type": "eq", "fun": f_2, "jac": f_2_grad},
         ],
         bounds=[(0, 1)] * nonzero_values.shape[0],
-        method=method,
         tol=tol,
-        options=dict(maxiter=25_000),
+        method=method,
+        options=options,
+        **minimize_kwargs,
     )
+    res = _optimzer_early_stopping_wrapper(f_optim)
 
     if not res.success:
-        _log.info(f"Optimization failed. Return values may not represent a balanced SAM")
+        _log.info("Optimization failed. Return values may not represent a balanced SAM")
 
     balanced_SAM = pd.DataFrame(
         f_X(res.x), index=normalized_sam.index, columns=normalized_sam.columns
