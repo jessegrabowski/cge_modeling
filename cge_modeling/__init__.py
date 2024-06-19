9e66d2b7979f9261835bf7b6f5fd7fac72298fa2
Jesse Grabowski <jessegrabowski@gmail.com>
2024-06-19 16:21:43 +0800

    Add logger

--- cge_modeling/__init__.py
+++ cge_modeling/__init__.py
@@ -3,6 +3,12 @@ from cge_modeling.base.primitives import Equation, Parameter, Variable
 from cge_modeling.plotting import plot_kateplot, plot_lines
 from cge_modeling.tools.output_tools import display_info_as_table, latex_print_equations
 
+import logging
+
+_log = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+
 __all__ = [
     "CGEModel",
     "Variable",
