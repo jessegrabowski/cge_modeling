83bfe2ac0a87d8221a832318e43fac49217cd90f
jessegrabowski <jessegrabowski@gmail.com>
2024-08-21 09:33:00 +0800

    Fix import order in init

--- cge_modeling/__init__.py
+++ cge_modeling/__init__.py
@@ -1,9 +1,9 @@
 import logging
 
-from cge_modeling.base.build import cge_model, compile_model
-from cge_modeling.base.cge import CGEModel
 from cge_modeling.base.primitives import Equation, Parameter, Variable
-from cge_modeling.compile.pytensor_tools import prod_to_no_zero_prod  #  noqa
+from cge_modeling.base.cge import CGEModel
+from cge_modeling.base.build import cge_model, compile_model
+from cge_modeling.compile.pytensor_tools import prod_to_no_zero_prod
 from cge_modeling.plotting import plot_bar, plot_kateplot, plot_lines
 from cge_modeling.tools.output_tools import display_info_as_table, latex_print_equations
 
