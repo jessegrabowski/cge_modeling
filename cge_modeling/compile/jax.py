bf7f60992e79e252ca0a0f25ddd8ffa3468134c0
Jesse Grabowski <jessegrabowski@gmail.com>
2024-08-27 10:05:28 +0800

    Improvements to jax function generation

--- cge_modeling/compile/jax.py
+++ cge_modeling/compile/jax.py
@@ -1,3 +1,5 @@
+import logging
+
 from collections.abc import Callable
 from functools import partial
 
@@ -9,20 +11,26 @@ from pytensor.graph import FunctionGraph
 from pytensor.link.jax.dispatch import jax_funcify
 
 from cge_modeling import CGEModel
-from cge_modeling.base.function_wrappers import return_array_from_jax_wrapper
+from cge_modeling.base.function_wrappers import (
+    return_array_from_jax_wrapper,
+    wrap_scipy_func_for_pytensor,
+)
 from cge_modeling.compile.constants import CompiledFunctions
+from cge_modeling.compile.euler import pytensor_euler_function_with_python_loop
 from cge_modeling.compile.pytensor import (
     cge_primitives_to_pytensor,
-    pytensor_euler_function_with_python_loop,
     validate_pytensor_parsing_result,
 )
-from cge_modeling.compile.pytensor_tools import flat_tensor_to_ragged_list
+from cge_modeling.compile.pytensor_tools import flat_tensor_to_ragged_list, rewrite_pregrad
+
+_log = logging.getLogger(__name__)
 
 
 def graph_to_jax_function(inputs, outputs):
     if not isinstance(outputs, list):
         outputs = [outputs]
 
+    rewrite_pregrad(outputs)
     fgraph = FunctionGraph(inputs=inputs, outputs=outputs, clone=True)
     fgraph.attach_feature(
         Supervisor(
@@ -31,28 +39,40 @@ def graph_to_jax_function(inputs, outputs):
             if not (hasattr(fgraph, "destroyers") and fgraph.has_destroyers([input]))
         )
     )
+
     mode.JAX.optimizer.rewrite(fgraph)
     jax_func = jax_funcify(fgraph)
 
-    return jax_func
+    def f_jax(*args, **kwargs):
+        return jax_func(*args, **kwargs)[0]
+
+    return f_jax
+
+
+def make_jax_system_and_jacobian(f, variables, parameters):
+    x_shapes = [x.type.shape for x in variables]
+    theta_shapes = [x.type.shape for x in parameters]
+
+    def f_sys_wrapped(x, theta):
+        xs = flat_tensor_to_ragged_list(x, x_shapes)
+        thetas = flat_tensor_to_ragged_list(theta, theta_shapes)
+
+        return f(*xs, *thetas)
+
+    jax_jac = jax.jacobian(f_sys_wrapped, 0)
+
+    return f_sys_wrapped, jax_jac
 
 
 def jax_euler_step(system, variables, parameters):
-    f_system_jax = graph_to_jax_function(inputs=[*variables, *parameters], outputs=[system])
+    f_system_jax = graph_to_jax_function(inputs=[*variables, *parameters], outputs=[system])  # noeq
+    f_sys_wrapped, f_jac = make_jax_system_and_jacobian(f_system_jax, variables, parameters)
 
     x_shapes = [x.type.shape for x in variables]
     theta_shapes = [x.type.shape for x in parameters]
 
     variable_names = [x.name for x in variables]
     parameter_names = [x.name for x in parameters]
-
-    def f_sys_wrapped(x, theta):
-        xs = flat_tensor_to_ragged_list(x, x_shapes)
-        thetas = flat_tensor_to_ragged_list(theta, theta_shapes)
-
-        return f_system_jax(*xs, *thetas)[0]
-
-    f_sys = jax.jit(f_sys_wrapped)
 
     def f_step(**kwargs):
         n_steps = kwargs.pop("n_steps")
@@ -69,9 +89,9 @@ def jax_euler_step(system, variables, parameters):
 
         step_size = (theta_final_vec - theta0_vec) / n_steps
 
-        A = jax.jacobian(f_sys, 0)(x_vec, theta_vec)
-        _, Bv = jax.jvp(lambda theta: f_sys(x_vec, theta), (theta_vec,), (step_size,))
-        step = -jax.scipy.linalg.solve(A, Bv)
+        A = f_jac(x_vec, theta_vec)
+        _, Bv = jax.jvp(lambda theta: f_sys_wrapped(x_vec, theta), (theta_vec,), (step_size,))
+        step = -jax.scipy.linalg.solve(A, Bv[:, None])[:, 0]
 
         x_next_vec = x_vec + step
         theta_next_vec = theta_vec + step_size
@@ -95,7 +115,7 @@ def jax_loss_grad_hessp(system, variables, parameters):
         xs = flat_tensor_to_ragged_list(x, x_shapes)
         thetas = flat_tensor_to_ragged_list(theta, theta_shapes)
 
-        return f_loss_jax(*xs, *thetas)[0]
+        return f_loss_jax(*xs, *thetas)
 
     grad = jax.grad(f_loss_wrapped, 0)
 
@@ -120,8 +140,11 @@ def compile_jax_cge_functions(
     system, variables, parameters, (cache, unpacked_cache) = cge_primitives_to_pytensor(cge_model)
     validate_pytensor_parsing_result(system, variables, parameters, cache)
 
+    _log.info("Compiling function to JAX")
+
     f_system = graph_to_jax_function(inputs=[*variables, *parameters], outputs=[system])
-    f_system = return_array_from_jax_wrapper(f_system)
+    _, _f_jac = make_jax_system_and_jacobian(f_system, variables, parameters)
+    f_system = return_array_from_jax_wrapper(jax.jit(f_system))
 
     # Optional functions
     f_jac = None
@@ -132,25 +155,30 @@ def compile_jax_cge_functions(
     f_euler = None
 
     if "root" in functions_to_compile:
-
-        def f_jac(x, theta):
-            return jnp.stack(jax.jacobian(f_system, 0)(*x, *theta))
+        f_jac = wrap_scipy_func_for_pytensor(
+            return_array_from_jax_wrapper(jax.jit(_f_jac)),
+            cge_model.variables,
+            cge_model.parameters,
+        )
 
     if "minimize" in functions_to_compile:
         # Symbolically compute loss function and derivatives
-        f_resid, f_grad, f_hess, f_hessp = jax_loss_grad_hessp(system, variables, parameters)
+        jax_functions = jax_loss_grad_hessp(system, variables, parameters)
+        f_resid, f_grad, f_hess, f_hessp = (
+            return_array_from_jax_wrapper(jax.jit(f)) for f in jax_functions
+        )
 
     if "euler" in functions_to_compile:
         # Compile the one-step linear approximation function used by the iterative Euler approximation
         # We don't need to save this because it's only used internally by the euler_approx function
-        f_step = jax_euler_step(system, variables, parameters)
+        f_step = jax.jit(jax_euler_step(system, variables, parameters))
 
         f_euler = partial(
             pytensor_euler_function_with_python_loop,
             cge_model=cge_model,
             f_step=f_step,
             f_system=f_system,
-            f_grad=f_grad,
+            f_grad=wrap_scipy_func_for_pytensor(f_grad, cge_model.variables, cge_model.parameters),
         )
 
-    return tuple(jax.jit(f) for f in [f_system, f_jac, f_resid, f_grad, f_hess, f_hessp, f_euler])
+    return f_system, f_jac, f_resid, f_grad, f_hess, f_hessp, f_euler
