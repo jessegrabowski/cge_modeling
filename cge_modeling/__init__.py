2bbbbbefbae00f9c18e0dc1994705402c450a1ab
Jesse Grabowski <48652735+jessegrabowski@users.noreply.github.com>
2025-05-25 22:20:35 +0800

    Use Hatch to build project (#51)
    * Use `hatch` to build sdist/wheel and `hatch-vcs` for versioning
    
    * Formatting
    
    * Add license
    
    * Add version fallback

--- cge_modeling/__init__.py
+++ cge_modeling/__init__.py
@@ -7,8 +7,7 @@ from cge_modeling.compile.pytensor_tools import prod_to_no_zero_prod
 from cge_modeling.plotting import plot_bar, plot_kateplot, plot_lines
 from cge_modeling.tools.output_tools import display_info_as_table, latex_print_equations
 
-from cge_modeling._version import get_versions
-
+from importlib.metadata import PackageNotFoundError, version
 
 _log = logging.getLogger("cge_modeling")
 if not logging.root.handlers:
@@ -17,8 +16,19 @@ if not logging.root.handlers:
         handler = logging.StreamHandler()
         _log.addHandler(handler)
 
-
-__version__ = get_versions()["version"]
+try:
+    __version__ = version("cge_modeling")
+except PackageNotFoundError:
+    try:
+        # Fallback for when the package is not installed, e.g., during development
+        from cge_modeling._version import __version__
+    except ImportError:
+        _log.warning(
+            "Could not determine package version, you're probably doing development work without having "
+            "installed the package. Call `hatch build --hooks-only` to generate the version file, or "
+            "install the package with `pip install .`."
+        )
+        __version__ = "0.0.0"
 
 __all__ = [
     "cge_model",
